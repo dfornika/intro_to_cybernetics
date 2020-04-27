9e8b67e28134c6afb343373fb944114d1dfdebdb
Dan Fornika <dfornika@gmail.com>
2020-04-26 19:10:53 -0700

    Add (empty) ch02 file and update ch03

--- intro_to_cybernetics/chapter_03.clj
+++ intro_to_cybernetics/chapter_03.clj
@@ -50,3 +50,43 @@
 (comment
   (ex-05-transform [3 -2 1]) ; => [8 -3 1]
   )
+
+;; Ex. 6:
+;; Arthur and Bill agree to have a gamble. Each is to divide his money into
+;; two equal parts, and at the umpire's signal each is to pass one part over
+;; to the other player. Each is then again to divide his new wealth into two
+;; equal parts and at a signal to pass a half to the other; and so on.
+;; Arthur started with 8/- and Bill with 4/-. Represent the initial operand by
+;; the vector [8, 4].
+;; Find, in any way you can, all its subsequent transforms.
+(defn ex-06-transform
+  [[a b]]
+  [(+ (/ a 2) (/ b 2))
+   (+ (/ b 2) (/ a 2))])
+
+(comment
+  (ex-06-transform [8 4]) ; => [6 6]
+  )
+
+;; Ex. 8:
+;; Charles and david decide to play a similar game except that each will
+;; hand over a sum equal to a half of what the *other* posesses.
+;; If they start with 30/- and 34/- respectively, what will happen to these
+;; quantities?
+(defn ex-07-transform
+  [[c d]]
+  [(+ (- c (/ d 2))
+      (/ c 2))
+   (+ (- d (/ c 2))
+      (/ d 2))])
+
+(comment
+  (ex-07-transform [30 34]) ; => [28 36]
+  (recursive-transform ex-07-transform [30 34] 5) ; => [[30 34] [28 36] [24 40] [16 48] [0 64]]
+  )
+
+;; Ex. 10:
+;; If, in Ex. 8, other sums of money had been started with, who in general would be the winner?
+(comment
+  (map ex-07-transform [[28 36]]) 
+  )
